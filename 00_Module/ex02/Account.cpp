806ee5d454083be485d9c5cc1962f5cc24f1993f
Diogo G <desktop-diogo>
2023-08-17 18:02:41 +0100

    created account

---
+++ 00_Module/ex02/Account.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hiper <hiper@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 17:53:03 by hiper             #+#    #+#             */
+/*   Updated: 2023/08/17 18:01:16 by hiper            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account( void )
+{
+    return ;
+} 
+
+int	Account::getNbAccounts( void )
+{
+    return (0);
+}
+
+int	Account::getTotalAmount( void )
+{
+    return (0);
+    
+}
+
+int	Account::getNbDeposits( void )
+{
+    return (0);
+    
+}
+
+int	Account::getNbWithdrawals( void )
+{
+    return (0);
+    
+}
+
+void Account::displayAccountsInfos( void )
+{
+    return ;
+    
+}
