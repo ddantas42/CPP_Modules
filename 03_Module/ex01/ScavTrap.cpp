6de03128e94110e84ffdcfa9f87b0406e6f8645f
Diogo G <desktop-diogo>
2023-09-04 16:27:48 +0100

    01/ex01 added OCF

--- 03_Module/ex01/ScavTrap.cpp
+++ 03_Module/ex01/ScavTrap.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScavTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ddantas- <ddantas-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hiper <hiper@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 03:20:22 by ddantas-          #+#    #+#             */
-/*   Updated: 2023/09/02 11:25:08 by ddantas-         ###   ########.fr       */
+/*   Updated: 2023/09/04 16:26:35 by hiper            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,33 @@
 
 ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
 {
-
 	this->name.assign(name);
 	this->hit_points = 100;
 	this->energy_points = 50;
 	this->attack_damage = 20;
-	
-	std::cout << "ScavTrap " << this->name << " Constructor called" << std::endl;
 
+	std::cout << "ScavTrap " << this->name << " Constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap( const ScavTrap &copy ) : ScavTrap(copy.name)
+{
+	this->name = copy.name;
+	this->ClapTrap::name = copy.name;
+	this->hit_points = copy.hit_points;
+	this->energy_points = copy.energy_points;
+	this->attack_damage = copy.attack_damage;
+}
+
+ScavTrap &ScavTrap::operator=( const ScavTrap &copy )
+{
+	std::cout << "Copy assignmente operator called" << std::endl;
+	this->name = copy.name;
+	this->ClapTrap::name = copy.name;
+	this->hit_points = copy.hit_points;
+	this->energy_points = copy.energy_points;
+	this->attack_damage = copy.attack_damage;
+
+	return (*this);
 }
 
 void ScavTrap::attack(const std::string& target)
--- 03_Module/ex01/main.cpp
+++ 03_Module/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ddantas- <ddantas-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hiper <hiper@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 02:46:05 by ddantas-          #+#    #+#             */
-/*   Updated: 2023/09/02 13:07:59 by ddantas-         ###   ########.fr       */
+/*   Updated: 2023/09/04 16:26:53 by hiper            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main()
 {
 	ScavTrap Jonh("Jonh");
 	ClapTrap Carreiras("Carreiras");
-	
+
 	Carreiras.beRepaired(20);
 	Jonh.beRepaired(10);
 	
