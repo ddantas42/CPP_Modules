c4adc1d024dd35d26d0f380e648b48bd37d6fd8d
Diogo Dantas goncalves <ddantas-@c2r8s3.42lisboa.com>
2023-09-06 14:39:46 +0100

    03/02 scavtrap updated

--- 03_Module/ex02/ScavTrap.hpp
+++ 03_Module/ex02/ScavTrap.hpp
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ddantas- <ddantas-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hiper <hiper@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 03:29:42 by ddantas-          #+#    #+#             */
-/*   Updated: 2023/09/02 11:23:00 by ddantas-         ###   ########.fr       */
+/*   Updated: 2023/09/04 15:46:12 by hiper            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCAVTRAP_HPP
-# define SCAVTRP_HPP
+# define SCAVTRAP_HPP
 
 # include "ClapTrap.hpp"
 # include <iostream>
@@ -24,7 +24,11 @@ class ScavTrap : public ClapTrap
 	public:
 	
 		ScavTrap( std::string name );
+		ScavTrap( const ScavTrap & copy);
 		~ScavTrap( void );
+		
+		ScavTrap &operator=(const ScavTrap &copy);
+		
 		void	guardGate( void );
 		void	attack(const std::string& target);
 };
