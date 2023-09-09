1378dba5e498ad4b54f6cfab3d63501043eaf902
Diogo G <desktop-diogo>
2023-09-09 02:57:48 +0100

    03/ex02 fixed

--- 03_Module/ex02/FragTrap.cpp
+++ 03_Module/ex02/FragTrap.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   FragTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ddantas- <ddantas-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hiper <hiper@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 03:20:22 by ddantas-          #+#    #+#             */
-/*   Updated: 2023/09/06 14:39:30 by ddantas-         ###   ########.fr       */
+/*   Updated: 2023/09/09 02:55:18 by hiper            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,17 @@ FragTrap::FragTrap( const FragTrap &copy ) : FragTrap(copy.name)
 	this->hit_points = copy.hit_points;
 	this->energy_points = copy.energy_points;
 	this->attack_damage = copy.attack_damage;
+}
+
+FragTrap &FragTrap::operator=( const FragTrap &copy )
+{
+	std::cout << "Copy assignmente operator called" << std::endl;
+	this->name = copy.name;
+	this->hit_points = copy.hit_points;
+	this->energy_points = copy.energy_points;
+	this->attack_damage = copy.attack_damage;
+
+	return (*this);
 }
 
 void FragTrap::attack(const std::string& target)
