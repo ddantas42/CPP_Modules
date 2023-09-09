e1ad18e219d988d1aa833f56d4c3183a56474a15
Diogo G <desktop-diogo>
2023-09-09 02:58:03 +0100

    03/ex01 fixed main

--- 03_Module/ex01/main.cpp
+++ 03_Module/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hiper <hiper@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 02:46:05 by ddantas-          #+#    #+#             */
-/*   Updated: 2023/09/09 02:45:01 by hiper            ###   ########.fr       */
+/*   Updated: 2023/09/09 02:50:35 by hiper            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ int main()
 	Jonh.attack("Carreiras");
 	Carreiras.takeDamage(20);
 	std::cout << std::endl;
+	
 	return (0);
 }
 
