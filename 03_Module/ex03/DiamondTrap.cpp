e5d03d78b5a32547ede3b83b8fd16c071c4e19a9
Diogo G <desktop-diogo>
2023-09-14 00:43:52 +0100

    dia

--- 03_Module/ex03/DiamondTrap.cpp
+++ 03_Module/ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: hiper <hiper@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 14:40:18 by ddantas-          #+#    #+#             */
-/*   Updated: 2023/09/13 15:15:37 by hiper            ###   ########.fr       */
+/*   Updated: 2023/09/13 15:16:41 by hiper            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@
 DiamondTrap::DiamondTrap(std::string name) : FragTrap(name), ScavTrap(name)
 {
     this->name = name + "_DiamondTrap";
-	this->attack_damage = FragTrap::attack_damage;
-	
     std::cout << "DiamondTrap " << this->name << " Constructor called" << std::endl;
 }
 
