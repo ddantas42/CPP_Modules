ec4ba70870a2708596660bc972c2a581f032bd66
Diogo G <desktop-diogo>
2023-10-06 23:10:44 +0100

    05/ex00 started

---
+++ 05_Module/ex00/Bureaucrat.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hiper <hiper@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 22:31:28 by hiper             #+#    #+#             */
+/*   Updated: 2023/10/06 23:10:24 by hiper            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+// Constructors
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : name(name), grade(grade)
+{
+	// if (grade < 1)
+	// 	throw Bureaucrat::GradeTooHighException();
+	// else if (grade > 150)
+	// 	throw Bureaucrat::GradeTooLowException();
+}
+
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy)
+{
+	(void) copy;
+}
+
+const std::string	&Bureaucrat::getName() const
+{return this->name;}
+
+int	Bureaucrat::getGrade() const
+{return this->grade;}
+
+// Destructor
+Bureaucrat::~Bureaucrat()
+{
+}
+
+
+// Operators
+Bureaucrat & Bureaucrat::operator=(const Bureaucrat &assign)
+{
+	(void) assign;
+	return *this;
+}
+
+std::ostream&	operator<<(std::ostream &out, const Bureaucrat& bureaucrat)
+{
+	out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << std::endl;
+	return out;
+}
---
+++ 05_Module/ex00/Bureaucrat.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hiper <hiper@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 22:31:26 by hiper             #+#    #+#             */
+/*   Updated: 2023/10/06 23:10:17 by hiper            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+
+class Bureaucrat
+{
+	public:
+		// Constructors
+		Bureaucrat(const std::string &name, int grade);
+		
+		Bureaucrat(const Bureaucrat &copy);
+		
+		// Destructor
+		~Bureaucrat();
+		
+		// Operators
+		Bureaucrat & operator=(const Bureaucrat &assign);
+
+		// void GradeTooLowException();
+		// void GradeTooHighException();
+
+		const std::string	&getName() const;
+		int					getGrade() const;
+		
+
+	private:
+		const std::string	name;	
+		int					grade;
+		Bureaucrat() {};
+};
+
+std::ostream&		operator<<(std::ostream &out, const Bureaucrat& bureaucrat);
+
+#endif
