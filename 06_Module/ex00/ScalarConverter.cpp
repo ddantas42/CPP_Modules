1816f2ce62263dc5c2b7b7a4d4511dde425d221c
Diogo <email>
2024-03-16 18:33:15 +0000

    Started 06

---
+++ 06_Module/ex00/ScalarConverter.cpp
@@ -0,0 +1,26 @@
+#include "ScalarConverter.hpp"
+
+// Constructors
+ScalarConverter::ScalarConverter()
+{
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &copy)
+{
+	(void) copy;
+}
+
+
+// Destructor
+ScalarConverter::~ScalarConverter()
+{
+}
+
+
+// Operators
+ScalarConverter & ScalarConverter::operator=(const ScalarConverter &assign)
+{
+	(void) assign;
+	return *this;
+}
+
