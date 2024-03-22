e2dba7c4260022a3ad1e4b512e1c95782a12cf51
Diogo Dantas goncalves <ddantas-@c2r4s5.42lisboa.com>
2024-03-22 19:52:23 +0000

    removing useless iterator

--- 08_Module/ex02/MutantStack.hpp
+++ 08_Module/ex02/MutantStack.hpp
@@ -26,26 +26,6 @@ class MutantStack
 		size_t size();
 		bool empty();
 
-		template <typename U>
-		class Iterator
-		{
-			public:
-				Iterator() {};
-				Iterator(T *ptr) : ptr(ptr) {};
-				Iterator(const Iterator &copy) : ptr(copy.ptr) {};
-				~Iterator() {};
-				Iterator & operator=(const Iterator &assign) {ptr = assign.ptr; return *this;};
-				bool operator!=(const Iterator &other) {return ptr != other.ptr;};
-				bool operator==(const Iterator &other) {return ptr == other.ptr;};
-				T & operator*() {return *ptr;};
-				Iterator & operator++() {ptr++; return *this;};
-				Iterator operator++(int) {Iterator tmp(*this); operator++(); return tmp;};
-				Iterator & operator--() {ptr--; return *this;};
-				Iterator operator--(int) {Iterator tmp(*this); operator--(); return tmp;};
-			private:
-				T *ptr;
-		};
-
 	private:
 		std::stack<T> stack;	
 };
