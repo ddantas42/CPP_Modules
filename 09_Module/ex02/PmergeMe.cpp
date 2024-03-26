910f62bdf7673b4e4a507742f7c30fda63b13242
Diogo Dantas goncalves <ddantas-@c2r6s3.42lisboa.com>
2024-03-26 22:38:47 +0000

    09 Pmerge segfault when 4 members

--- 09_Module/ex02/PmergeMe.cpp
+++ 09_Module/ex02/PmergeMe.cpp
@@ -35,6 +35,7 @@ double PmergeMe::v_sort(int ac, char **av)
 	for (int n = 1; n < ac; n++)
 		X.push_back(std::atoi(av[n]));
 
+
 	for (std::vector<int>::iterator it = X.begin(); it != X.end(); it++)
 	{
 		for (std::vector<int>::iterator it2 = X.begin(); it2 != X.end(); it2++)
@@ -69,7 +70,7 @@ double PmergeMe::v_sort(int ac, char **av)
 			S.insert(std::lower_bound(S.begin(), S.end(), *(i + 1)), *(i + 1));
 			remains.push_back(*(i+1));
 		}
-		if (i + 2 == X.end())
+		if (i + 1 == X.end() || i + 2 == X.end())
 			break;
 		i += 2;
 	}
@@ -84,18 +85,80 @@ double PmergeMe::v_sort(int ac, char **av)
 	}
 
 	S.insert(S.begin(), smallest_left); // 4 paired with smallest
-	// S.insert(S.begin(), *std::min_element(X.begin(), X.end()));
 
 	for (std::vector<int>::iterator i = X.begin(); i != X.end(); i++) // 5
 		S.insert(std::lower_bound(S.begin(), S.end(), *i), *i);
 
 	end = clock();
+	// 	std::cout << "Sorted Integeres: ";
+	// for (std::vector<int>::iterator s = S.begin(); s != S.end(); s++)
+	// 	std::cout << *s << " ";
+	// std::cout << std::endl;
+	return ((double) (end - start)) / CLOCKS_PER_SEC;
+
+}
+
+double PmergeMe::l_sort(int ac, char **av)
+{
+	clock_t start, end;
+	start = clock();
+	std::list<int> X;
+	std::list<int> S;
+	std::list<int> remains;
+	std::list<int>::iterator i;
+
+	for (int n = 1; n < ac; n++)
+		X.push_back(std::atoi(av[n]));
+	i = X.begin();
+	int smallest_left = *i;
+	while (X.size() >= 2) // 1, 2, 3
+	{
+		if (*i > *X.end())
+		{
+			if (*X.end() < smallest_left)
+				smallest_left = *X.end();
+			S.insert(std::lower_bound(S.begin(), S.end(), *i), *i);
+			remains.push_back(*X.end());
+			X.pop_front();
+			X.pop_back();
+		}
+		else
+		{
+			if (*i < smallest_left)
+				smallest_left = *i;
+
+			S.insert(std::lower_bound(S.begin(), S.end(), *X.end()), *X.end());
+			remains.push_back(*i);
+			X.pop_front();
+			X.pop_back();
+
+		}
+
+		std::cout << "Current List: ";
+		for (std::list<int>::iterator s = S.begin(); s != S.end(); s++)
+			std::cout << *s << " ";
+		std::cout << std::endl;
+
+		if (X.size() > 2)
+		{
+			// remains.push_back(*i);
+			// X.pop_front();
+			break ;
+		}
+		i = X.begin();
+	}
+	// S.insert(S.begin(), smallest_left); // 4 paired with smallest
+	// X.assign(remains.begin(), remains.end());
+	
+	// for (std::list<int>::iterator i = X.begin(); i != X.end(); i++) // 5
+	// 	S.insert(std::lower_bound(S.begin(), S.end(), *i), *i);
+
+	end = clock();
 		std::cout << "Sorted Integeres: ";
-	for (std::vector<int>::iterator s = S.begin(); s != S.end(); s++)
+	for (std::list<int>::iterator s = S.begin(); s != S.end(); s++)
 		std::cout << *s << " ";
 	std::cout << std::endl;
 	return ((double) (end - start)) / CLOCKS_PER_SEC;
-
 }
 
 void PmergeMe::sort(int ac, char **av)
@@ -106,11 +169,13 @@ void PmergeMe::sort(int ac, char **av)
 		return ;
 	}
 
+
 	std::cout << "Unsorted Integeres: ";
 	int i = 1;
 	for (; i < ac; i++)
 		std::cout << av[i] << " ";
 	std::cout << std::endl;
+
 	float v_time = 0; 
 	try {
 		v_time = v_sort(ac, av);
@@ -118,9 +183,8 @@ void PmergeMe::sort(int ac, char **av)
 		std::cout << e << std::endl; 
 		return ;
 	}
+
+	float l_time = l_sort(ac, av);
 	std::cout << "Time to process a range of " << i << " elements with std::vector : " << v_time << std::endl; 
-	// std::cout << "Time to process a range of " << i << "elements with std::list : " << t_time << std::endl; 
-
-
-
+	std::cout << "Time to process a range of " << i << " elements with std::list : " << l_time << std::endl; 
 }
